93cb3d724cd1ece04540a5deb6ed06681a4a5315
chanpreet <singh.cpreet2010@outlook.com>
2020-08-20 16:36:28 -0400

    Git Upload

--- JS/music.js
+++ JS/music.js
@@ -82,7 +82,7 @@ function randRange(maxNum, minNum) {
 }
 
 function createRain() {
-        $('#rain').append('<img src="Images/rain1.jpg" alt ="rain" class="night img-fluid img-responsive"></img>');
+        $('#rain').append('<img src="Images/rain1.jpg" alt ="rain" class="night img-fluid img-responsive"/>');
 
 //    $("#rain").css({
 //        "background-image": 'url(Images/rain1.jpg)',
@@ -98,7 +98,7 @@ function createRain() {
 //'background-size': 'cover',
 //    });
     for (i = 0; i < nbDrop; i++) {
-        var dropLeft = randRange(0, 3000);
+        var dropLeft = randRange(0, 2000);
         var dropTop = randRange(-1000, 1500);
         $('#rain').append('<div class="drop" id="drop' + i + '"></div>');
         $('#drop' + i).css('left', dropLeft);
